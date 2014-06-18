d8bc5dba2dcad3588e69cac8e0f39bf0faefe12c
Pepa P <emp3rr@gmail.com>
2014-06-18 18:29:18 +0200

    new Colors option in menu
    and also adjusted for new utils.Rules and utils.Appearance classes

--- src/form/Form.java
+++ src/form/Form.java
@@ -14,13 +14,12 @@ import javax.swing.KeyStroke;
 
 import data.ScoreManager;
 import dialogs.*;
+import utils.Appearance;
+import utils.Rules;
 
 public class Form extends JFrame {
 
 	private static final long serialVersionUID = 845222177524994234L;
-	private static final int EASY = 13;
-	private static final int MEDIUM = 8;
-	private static final int HARD = 5;
 	
 	private JGame game;
 	private ScoreManager scoreManager;
@@ -29,24 +28,23 @@ public class Form extends JFrame {
 	private JMenu menuSize;
 	private JMenu menuDifficulty;
 	private JMenu menuScore;
-	private int heightMenuDifference = 0;
+	private int actualSize;
 	
 	public Form() {
 		this.setDefaultCloseOperation(Form.EXIT_ON_CLOSE);
 		this.setTitle("Gray Sweeper");
 		
 		scoreManager = new ScoreManager();
-		game = new JGame(16, 16, MEDIUM, this);
+		game = new JGame(Rules.MEDIUM_X, Rules.MEDIUM_Y, Rules.MEDIUM, this);
+		actualSize = Rules.SIZE_MEDIUM;
 		this.getContentPane().add(game);
 
-		if (System.getProperty("os.name").equals("Mac OS X")) {
+		if (System.getProperty("os.name").equals("Mac OS X"))
 			System.setProperty("apple.laf.useScreenMenuBar", "true");
-			heightMenuDifference = 22;
-		}
 		initializeMenu();
 		
 		this.setJMenuBar(mainMenu);
-		this.setPreferredSize(new Dimension(482, 556 - heightMenuDifference));
+		this.setPreferredSize(new Dimension(482, 534));
 		this.setLocation(300, 160);
 		this.setResizable(false);
 		pack();
@@ -61,7 +59,7 @@ public class Form extends JFrame {
 		
 		if (dialog.getResult() != null && !dialog.getResult().equals("")) { 
 			int score = (game.getMinutes() * 100) + game.getSeconds();
-			scoreManager.writeScore(score, dialog.getResult(), dif);
+			scoreManager.writeScore(score, dialog.getResult(), dif, actualSize);
 		}
 	}
 	
@@ -110,6 +108,32 @@ public class Form extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) { aboutDialog(); }
 		});
+		// colors
+		JMenu menuItemColors = new JMenu("Colors");
+		final JCheckBoxMenuItem menuItemColorGray = new JCheckBoxMenuItem("Gray");
+		final JCheckBoxMenuItem menuItemColorReverse = new JCheckBoxMenuItem("Reverse");
+		menuItemColorGray.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Appearance.setGrayColours();
+				menuItemColorGray.setSelected(true);
+				menuItemColorReverse.setSelected(false);
+			}
+		});
+		menuItemColorReverse.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Appearance.setReverseGrayColours();
+				menuItemColorReverse.setSelected(true);
+				menuItemColorGray.setSelected(false);
+			}
+		});
+		menuItemColorGray.setSelected(true);
+		menuItemColors.add(menuItemColorGray);
+		menuItemColors.add(menuItemColorReverse);
+		
+		//
+		
 		JMenuItem menuItemExit = new JMenuItem("Exit");
 		menuItemExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
 		menuItemExit.addActionListener(new ActionListener() {
@@ -117,6 +141,7 @@ public class Form extends JFrame {
 			public void actionPerformed(ActionEvent e) { setVisible(false); dispose(); System.exit(0); }
 		});
 		menuGame.add(menuItemNew);
+		menuGame.add(menuItemColors);
 		menuGame.add(menuItemAbout);
 		menuGame.add(menuItemExit);
 		mainMenu.add(menuGame);
@@ -128,19 +153,19 @@ public class Form extends JFrame {
 		menuItemEasy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.CTRL_MASK));
 		menuItemEasy.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) { game.setDifficulty(EASY); game.newGame(); 
+			public void actionPerformed(ActionEvent e) { game.setDifficulty(Rules.EASY); game.newGame(); 
 				menuItemEasy.setSelected(true); menuItemMedium.setSelected(false); menuItemHard.setSelected(false);}
 		});
 		menuItemMedium.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_2, ActionEvent.CTRL_MASK));
 		menuItemMedium.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) { game.setDifficulty(MEDIUM); game.newGame(); 
+			public void actionPerformed(ActionEvent e) { game.setDifficulty(Rules.MEDIUM); game.newGame(); 
 				menuItemMedium.setSelected(true); menuItemEasy.setSelected(false); menuItemHard.setSelected(false);}
 		});
 		menuItemHard.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_3, ActionEvent.CTRL_MASK));
 		menuItemHard.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) { game.setDifficulty(HARD); game.newGame(); 
+			public void actionPerformed(ActionEvent e) { game.setDifficulty(Rules.HARD); game.newGame(); 
 				menuItemHard.setSelected(true); menuItemEasy.setSelected(false); menuItemMedium.setSelected(false);}
 		});
 		menuItemMedium.setSelected(true);
@@ -155,20 +180,38 @@ public class Form extends JFrame {
 		menuItemSizeSmall.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK));
 		menuItemSizeSmall.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) { game.newGame(16, 8); setSize(new Dimension(482, 316 - heightMenuDifference));
-				menuItemSizeSmall.setSelected(true); menuItemSizeMedium.setSelected(false); menuItemSizeLarge.setSelected(false);}
+			public void actionPerformed(ActionEvent e) {
+				game.newGame(Rules.SMALL_X, Rules.SMALL_Y);
+				setSize(new Dimension(482, 294));
+				actualSize = Rules.SIZE_SMALL;
+				menuItemSizeSmall.setSelected(true);
+				menuItemSizeMedium.setSelected(false);
+				menuItemSizeLarge.setSelected(false);
+			}
 		});
 		menuItemSizeMedium.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_2, ActionEvent.ALT_MASK));
 		menuItemSizeMedium.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) { game.newGame(16, 16); setSize(new Dimension(482, 556 - heightMenuDifference));
-				menuItemSizeMedium.setSelected(true); menuItemSizeSmall.setSelected(false); menuItemSizeLarge.setSelected(false);}
+			public void actionPerformed(ActionEvent e) {
+				game.newGame(Rules.MEDIUM_X, Rules.MEDIUM_Y);
+				setSize(new Dimension(482, 534));
+				actualSize = Rules.SIZE_MEDIUM;
+				menuItemSizeMedium.setSelected(true);
+				menuItemSizeSmall.setSelected(false);
+				menuItemSizeLarge.setSelected(false);
+			}
 		});
 		menuItemSizeLarge.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_3, ActionEvent.ALT_MASK));
 		menuItemSizeLarge.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) { game.newGame(32, 16); setSize(new Dimension(962, 556 - heightMenuDifference));
-				menuItemSizeLarge.setSelected(true); menuItemSizeSmall.setSelected(false); menuItemSizeMedium.setSelected(false);}
+			public void actionPerformed(ActionEvent e) {
+				game.newGame(Rules.LARGE_X, Rules.LARGE_Y);
+				setSize(new Dimension(962, 534));
+				actualSize = Rules.SIZE_LARGE;
+				menuItemSizeLarge.setSelected(true);
+				menuItemSizeSmall.setSelected(false);
+				menuItemSizeMedium.setSelected(false);
+			}
 		});
 		menuItemSizeMedium.setSelected(true);
 		menuSize.add(menuItemSizeSmall);
@@ -183,21 +226,22 @@ public class Form extends JFrame {
 		JMenuItem menuItemScoreReset = new JMenuItem("Reset tables"); 
 		menuItemScoreEasy.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) { scoreDialog(EASY); }
+			public void actionPerformed(ActionEvent e) { scoreDialog(Rules.EASY); }
 		});
 		menuItemScoreMedium.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) { scoreDialog(MEDIUM); }
+			public void actionPerformed(ActionEvent e) { scoreDialog(Rules.MEDIUM); }
 		});
 		menuItemScoreHard.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) { scoreDialog(HARD); }
+			public void actionPerformed(ActionEvent e) { scoreDialog(Rules.HARD); }
 		});
 		menuItemScoreReset.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) { 
-				if (yesNoDialog())
-					scoreManager.fillTable(); 
+				if (yesNoDialog()) {
+					scoreManager.resetTables();
+				}
 			}
 		});
 		menuScore.add(menuItemScoreEasy);
